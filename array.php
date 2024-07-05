e70ec2f6a9f64f2b5f1fe77c45e4e62583a79732
yanglong <2723659854@qq.com>
2024-07-05 11:26:58 +0800

    测试map数据交换

--- array.php
+++ array.php
@@ -6,6 +6,8 @@ use Swoole\Thread\ArrayList;
 
 $args = Thread::getArguments();
 if (empty($args)) {
+    var_dump("主线程");
+    var_dump(Swoole\Thread::getTsrmInfo());
     $list = new ArrayList;
     $thread = new Thread(__FILE__, 1, $list);
     sleep(1);
@@ -15,6 +17,8 @@ if (empty($args)) {
     $list[] = $class;
     $thread->join();
 } else {
+    var_dump("子线程");
+    var_dump(Swoole\Thread::getTsrmInfo());
     $list = $args[1];
     sleep(2);
     var_dump($list[0]);
--- map.php
+++ map.php
@@ -11,9 +11,21 @@ if (empty($args)) {
     $class = new stdClass();
     $class->name = "张三556565";
     $map['test'] = $class;
+    $map->add('demo','lucy');
+    var_dump("子线程开始执行");
     $thread->join();
+    var_dump("子线程执行完成");
 } else {
     $map = $args[1];
     sleep(2);
     var_dump($map['test']);
+    var_dump($map['demo']);
+    var_dump($map->keys());
+    var_dump($map->count());
+    /** values方法报错 Fatal error: Uncaught Error: Call to undefined method Swoole\Thread\Map::values() in /var/www/swoole/map.php:23*/
+    //var_dump($map->values());
+    /** toArray方法报错 */
+    //var_dump($map->toArray());
+    var_dump($map->clean());
+    var_dump($map->keys());
 }
